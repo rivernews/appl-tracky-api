52d96ca4ee5c78406fa787cc9da8f2e2653a33ba
Shaung Cheng <shaungc@umich.edu>
2019-03-01 19:55:17 -0500

    fix deploy script

--- delpoy.sh
+++ delpoy.sh
@@ -5,7 +5,9 @@ then
     git push
 fi
 
-$(aws ecr get-login --no-include-email --region us-east-2)
+cd django && \
+source ./venv/bin/activate && \
+$(aws ecr get-login --no-include-email --region us-east-2) && \
 docker-compose up -d --build --remove-orphans && \
 docker-compose push && echo Success && return
 
