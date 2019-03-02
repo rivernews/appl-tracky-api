3c2b09baf5d2154ead18c9a81cc68a536425f36d
Shaung Cheng <shaungc@umich.edu>
2019-03-02 00:35:11 -0500

    trigger terraform to update ecs service and task def

--- terraform/ecs_service.tf
+++ terraform/ecs_service.tf
@@ -4,6 +4,10 @@ resource "aws_ecs_service" "test-ecs-service" {
   task_definition = "${aws_ecs_task_definition.test.family}:${max("${aws_ecs_task_definition.test.revision}", "${data.aws_ecs_task_definition.test.revision}")}"
   desired_count   = 1
   iam_role        = "${aws_iam_role.ecs-service-role.name}"
+  
+  # allow service interruption upon new deployment and only run one instance of task at max: https://stackoverflow.com/a/46029034/9814131
+  deployment_minimum_healthy_percent = 0
+  deployment_maximum_percent = 100
 
   load_balancer {
     target_group_arn = "${aws_alb_target_group.http.id}"
