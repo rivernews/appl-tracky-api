0e2bfe7ba18bd76f81637290004c805775e5094b
Shaung Cheng <shaungc@umich.edu>
2019-03-01 20:59:05 -0500

    fix cloudwatch group setting

--- delpoy.sh
+++ delpoy.sh
@@ -10,6 +10,11 @@ source ./venv/bin/activate && \
 cd .. && \
 $(aws ecr get-login --no-include-email --region us-east-2) && \
 docker-compose up -d --build --remove-orphans && \
-docker-compose push && echo Success! ECR image ready. && return
+docker-compose push && echo "SUCESS! ECR image ready." && \
+echo "Now running orchestration tool to update service..." && \
+cd terraform && \
+terraform apply && \
+cd .. && \
+echo "SUCESS! Go to the url and check it out." && return
 
 echo ERROR: see above message.
