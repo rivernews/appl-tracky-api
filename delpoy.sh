a73710b0d1233f916752f6ad6c7e6a7d6ba3728f
Shaung Cheng <shaungc@umich.edu>
2019-03-01 19:57:56 -0500

    fix local deploy issue

--- delpoy.sh
+++ delpoy.sh
@@ -7,6 +7,7 @@ fi
 
 cd django && \
 source ./venv/bin/activate && \
+cd .. && \
 $(aws ecr get-login --no-include-email --region us-east-2) && \
 docker-compose up -d --build --remove-orphans && \
 docker-compose push && echo Success! ECR image ready. && return
