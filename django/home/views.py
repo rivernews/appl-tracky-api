c1eb1b637c03eff87f87a78a469afe4fb71aeb85
Shaung Cheng <shaungc@umich.edu>
2019-03-02 00:14:56 -0500

    try to fix: allow CORS

--- django/home/views.py
+++ django/home/views.py
@@ -4,3 +4,7 @@ from django.views import generic
 # Create your views here.
 class HomePageView(generic.TemplateView):
     template_name = 'home/home.html'
+
+    def get(self, request, *args, **kwargs):
+        print("Hey! Home Page View is reached!")
+        return super().get(request, *args, **kwargs)
