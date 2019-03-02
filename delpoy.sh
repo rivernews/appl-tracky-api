b5c8141226c00597b82d3a6af6bafc70a792f70b
Shaung Cheng <shaungc@umich.edu>
2019-03-01 19:53:03 -0500

    update docker img

---
+++ delpoy.sh
@@ -0,0 +1,12 @@
+if [[ $1 != "" ]];
+then
+    git add .
+    git commit -m "${1}"
+    git push
+fi
+
+$(aws ecr get-login --no-include-email --region us-east-2)
+docker-compose up -d --build --remove-orphans && \
+docker-compose push && echo Success && return
+
+echo ERROR: see above message.
