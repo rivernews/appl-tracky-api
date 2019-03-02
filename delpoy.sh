72348ac20c33f05d7e2da3ed3433196a3f95c04b
Shaung Cheng <shaungc@umich.edu>
2019-03-02 03:11:49 -0500

    test docker-compose build exit code

--- delpoy.sh
+++ delpoy.sh
@@ -16,7 +16,7 @@ $(aws ecr get-login --no-include-email --region us-east-2) && \
 NEW_IMAGE_TAG=$(git rev-parse --short HEAD) && \
 echo "INFO: Git short hash is ${NEW_IMAGE_TAG}, we will use this as new image tag." && \
 source .env && echo "INFO: Sourced environment file. ${AWS_ECR_NGINX_REPO_URI}:latest" && \
-docker-compose --log-level DEBUG --build --remove-orphans && \
+docker-compose --log-level DEBUG build --remove-orphans && \
 echo "INFO: docker-compose build complete." && \
 docker tag "${AWS_ECR_NGINX_REPO_URI}:latest" "${AWS_ECR_NGINX_REPO_URI}:${NEW_IMAGE_TAG}" && echo "INFO: finish tagging ${AWS_ECR_NGINX_REPO_URI}:${NEW_IMAGE_TAG}" && \
 docker tag "${AWS_ECR_WEB_REPO_URI}:latest" "${AWS_ECR_WEB_REPO_URI}:${NEW_IMAGE_TAG}" && echo "INFO: finish tagging ${AWS_ECR_WEB_REPO_URI}:${NEW_IMAGE_TAG}" && \
