846f117b291a04ab92d51281095191d1d30f1142
Shaung Cheng <shaungc@umich.edu>
2019-04-14 19:43:14 -0400

    try to address #1

--- django/entrypoint.sh
+++ django/entrypoint.sh
@@ -17,6 +17,7 @@ fi
 
 # this will only run on a fresh provision and the following.
 # if you do `flush`, then this `migrate` will not work
+python manage.py makemigrations
 python manage.py migrate
 
 python manage.py collectstatic --clear --noinput
