0a83d505b63a53bd98a129ffc83c5788532f2208
Shaung Cheng <shaungc@umich.edu>
2019-04-15 21:15:20 -0700

    fix setting env to .get

--- django/django_server/settings.py
+++ django/django_server/settings.py
@@ -241,10 +241,10 @@ SOCIAL_AUTH_PIPELINE = (
 # Django email setup
 SERVER_EMAIL = ''
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.environ['EMAIL_HOST']
-EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
-EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
-EMAIL_PORT = int(os.environ['EMAIL_PORT'])
+EMAIL_HOST = os.getenv('EMAIL_HOST', '')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', ''))
 EMAIL_USE_TLS = True
 EMAIL_SUBJECT_PREFIX = '[Test mail]'
 
