a589609a6e3b2e0f80fde19b94073226b6a6c340
Shaung <shaungc@umich.edu>
2019-10-05 12:40:59 -0700

    add comments for social auth resp field, #30

--- django/api/pipelines.py
+++ django/api/pipelines.py
@@ -33,7 +33,7 @@ def update_user_avatar(*args, **kwargs):
     kwargs = {
         'response': {
             'access_token':'ya29.GlvlBvrYj6xYOKQH2fadQWs0t7X4DDAyV5iISOb9rOBJFnu9FysdCVn36lz8yqueYIrXV9D7SCeXjUPh6G_bXY8HRJfm8XZNHivOFsapJlCISpwHzrUDUSePvEDC', 
-            'expires_in': 3598, 
+            'expires_in': 3598, # SECONDS_TO_EXPIRATION, see https://developers.google.com/actions/identity/google-sign-in-oauth
             'scope': 'https://www.googleapis.com/auth/userinfo.profile openid https://www.googleapis.com/auth/userinfo.email', 
             'token_type': 'Bearer', 
             'id_token': 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjZmNjc4MWJhNzExOTlhNjU4ZTc2MGFhNWFhOTNlNWZjM2RjNzUyYjUiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJhY2NvdW50cy5nb29nbGUuY29tIiwiYXpwIjoiNzMyOTg4NDk4ODQ4LXZ1aGQ2ZzYxYm5scWUzNzJpM2w1cGJwbmVydGV1Nm5hLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwiYXVkIjoiNzMyOTg4NDk4ODQ4LXZ1aGQ2ZzYxYm5scWUzNzJpM2w1cGJwbmVydGV1Nm5hLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTE0OTM0MjAzMzc1Mzc5ODQyOTk2IiwiaGQiOiJ1bWljaC5lZHUiLCJlbWFpbCI6InNoYXVuZ2NAdW1pY2guZWR1IiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImF0X2hhc2giOiJTRmNhWVRiUXlPdS1fejZ4ZE9zU1B3IiwiaWF0IjoxNTU0NjcyNTM4LCJleHAiOjE1NTQ2NzYxMzh9.b13pvrrHNMU2Pe_5co4SaVcGM9YtLsxB9qHAkWWK5wIBGRlP50FrrTN6oH2-9XaTRDQ9Gy3QBWYz5LQE_aM8ADwfD2UwVH56H48PIqb62Dlps9O44Lm17IB9-LQgRC7rP-tB5LSN59rjJfMp66gwQacrpejE_A3JKhNmZB1_X284m0nYhAYjwnADKe3NnvOZjTpQHwlf7dSvD59tVRq-S1dEldlWWTn28lOLQyj9D8TIqRjg7lkaeObVFQWDuzE56c-l4MgjmADvzshXZEr3dIQwLCexb5FnRDuIuEVt2Sn6R172OKLVXYJwihe0GZb5YzrVbhM_EZWl59q1jiT98w', 
