656d5c45e568d92a65a3e62a72641b26ddebc715
Shaung <shaungc@umich.edu>
2019-12-19 16:52:47 -0800

    refactor ownership filter for better reuse

--- django/api/filters.py
+++ django/api/filters.py
@@ -6,17 +6,28 @@ from rest_framework import serializers
 from django.core.exceptions import FieldDoesNotExist
 from django.db.models import Q
 
-def generic_ownership_filter_queryset(request, queryset):
-    try:
-        if not request.user.is_superuser and queryset.model._meta.get_field('user'):
-            return queryset.filter(
-                Q(user__isnull=True) |
-                Q(user__isnull=False) & Q(user=request.user)
-            )
-    except FieldDoesNotExist:
-        pass
+
+def ownership_filter_queryset(func):
+    def wrapper(*args, **kwargs):
+        if len(args) == 1:
+            request = args[0]
+        elif len(args) > 1:
+            request = args[1]
+        
+        queryset = func(*args, **kwargs)
+        
+        try:
+            if (not request.user.is_superuser) and queryset.model._meta.get_field('user'):
+                return queryset.filter(
+                    Q(user__isnull=True) |
+                    Q(user__isnull=False) & Q(user=request.user)
+                )
+        except FieldDoesNotExist:
+            pass
+        
+        return queryset
     
-    return queryset
+    return wrapper
 
 
 class LabelFilter(filters.FilterSet):
@@ -25,12 +36,9 @@ class LabelFilter(filters.FilterSet):
         fields = {'text': ['exact']}
 
 
+@ownership_filter_queryset
 def company_label_filter_get_queryset(request):
-    return generic_ownership_filter_queryset(
-        request,
-        models.Label.objects.all()
-    )
-
+    return models.Label.objects.all()
 
 class CompanyFilter(filters.FilterSet):
     labels = filters.RelatedFilter(LabelFilter, field_name='labels', queryset=company_label_filter_get_queryset)
@@ -45,7 +53,6 @@ class OwnershipFilterBackend(filter_backends.RestFrameworkFilterBackend):
     Filter that only allows users to see their own objects, or objects w/o a specific ownership.
     """
 
+    @ownership_filter_queryset
     def filter_queryset(self, request, queryset, view):
-        queryset = super().filter_queryset(request, queryset, view)
-
-        return generic_ownership_filter_queryset(request, queryset)
+        return super().filter_queryset(request, queryset, view)
