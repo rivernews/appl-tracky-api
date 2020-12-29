f5c465f9c237ab557790f7084b94673c3d29ab1e
Shaung Cheng <shaungc@umich.edu>
2020-12-29 08:35:01 +0000

    separate filter from rest and django-filter to avoid mixing

--- django/api/filters.py
+++ django/api/filters.py
@@ -1,10 +1,12 @@
 from . import models
-import rest_framework_filters as filters
+import rest_framework_filters
 import rest_framework_filters.backends as filter_backends
 from rest_framework import serializers
 
 from django.core.exceptions import FieldDoesNotExist
 from django.db.models import Q
+
+from django_filters import OrderingFilter, FilterSet
 
 
 def ownership_filter_queryset(func):
@@ -36,7 +38,7 @@ def ownership_filter_queryset(func):
     return wrapper
 
 
-class LabelFilter(filters.FilterSet):
+class LabelFilter(rest_framework_filters.FilterSet):
     class Meta:
         model = models.Label
         fields = {'text': ['exact']}
@@ -46,26 +48,14 @@ class LabelFilter(filters.FilterSet):
 def company_label_filter_get_queryset(request):
     return models.Label.objects.all()
 
-class CompanyFilter(filters.FilterSet):
-    labels = filters.RelatedFilter(filterset=LabelFilter, field_name='labels', queryset=company_label_filter_get_queryset)
+class CompanyFilter(rest_framework_filters.FilterSet):
+    labels = rest_framework_filters.RelatedFilter(filterset=LabelFilter, field_name='labels', queryset=company_label_filter_get_queryset)
 
     class Meta:
         model = models.Company
         fields = {
-            'labels': ['isnull'],
-
-            # even if `LabelFilter` specified `text` field, seems graphene-django cannot recognize it,
-            # so we have to explicitly specify here again
-            # see SO question (graphene-django not supporting nested field)
-            # https://stackoverflow.com/questions/49326217/graphene-django-nested-filters-relay
-            'labels__text': ['exact']
+            'labels': ['isnull']
         }
-    
-    # added for graphene-django
-    # because it does not support global filter backend
-    @property
-    def qs(self):
-        return super().qs.filter(user=self.request.user)
 
 
 class OwnershipFilterBackend(filter_backends.RestFrameworkFilterBackend):
@@ -76,3 +66,33 @@ class OwnershipFilterBackend(filter_backends.RestFrameworkFilterBackend):
     @ownership_filter_queryset
     def filter_queryset(self, request, queryset, view):
         return super().filter_queryset(request, queryset, view)
+
+
+# Below is for GraphQL
+
+
+class GraphQLCompanyFilter(FilterSet):
+    class Meta:
+        model = models.Company
+        fields = {
+            'labels': ['isnull'],
+
+            # graphene-django uses django-filter, which does not have `RelatedFilter`
+            # so we have to explicitly specify relational fields here
+            # see SO question (graphene-django not supporting nested filter)
+            # https://stackoverflow.com/questions/49326217/graphene-django-nested-filters-relay
+            'labels__text': ['exact']
+        }
+    
+    order_by = OrderingFilter(fields=(
+        ('modified_at',)
+    ))
+    
+    # added for graphene-django
+    # because it does not support global filter backend like DRF
+    @property
+    def qs(self):
+        queryset = super().qs
+        if self.request.user.is_superuser:
+            return queryset
+        return queryset.filter(user=self.request.user)
